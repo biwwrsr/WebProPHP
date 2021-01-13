016c3804af5f9d5f7316e5e6fc969d6532c0cba0
biwwrsr <6206021620132@fitm.kmutnb.ac.th>
2021-01-13 11:08:36 +0700

    create Lab4-1.php

---
+++ Lab4-1.php
@@ -0,0 +1,24 @@
+<html>
+<head><title>การก าหนดค่าตัวแปร</title></head>
+<body>
+<?php
+$x = 100;
+$y = "200";
+$z = '300';
+$add = $x + $y;
+$sub = $z - $y;
+$K = $x * $z;
+$H = $K / $z;
+$messageadd = "ผลบวก ".$x."+".$y." = ".$add;
+$messagesub = "ผลลบ ".$z."-".$x." = ".$sub;
+$messageK = "ผลคูณ ".$y."*".$z." = ".$K;
+$messageH = "ผลบวก ".$K."%".$z." = ".$H;
+
+echo $messageadd;  echo "<br />";
+echo $messagesub;  echo "<br />";
+echo $messageK;  echo "<br />";
+echo $messageH;  echo "<br />";
+
+?>
+</body>
+</html>
