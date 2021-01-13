9570b884244ebea72869b48ce8866afb1588a8e9
biwwrsr <6206021620132@fitm.kmutnb.ac.th>
2021-01-13 10:03:24 +0700

    create hello.php

---
+++ hello.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>hello</title>
+    </head>
+    <body>
+        <?php
+        echo "<h1>Hello</h1><br>";
+        echo "<h2>Prachinburi</h2>";
+        ?>
+    </body>
+</html>
