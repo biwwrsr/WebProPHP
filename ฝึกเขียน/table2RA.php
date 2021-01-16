1ca674fd7d7f44e5ed308050a9b2ffd6a4a4207f
biwwrsr <6206021620132@fitm.kmutnb.ac.th>
2021-01-16 20:53:44 +0700

    table2RA

---
+++ ฝึกเขียน/table2RA.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+<title>ตารางเรียน IT2RA </title>
+</head>
+<body>
+<center>
+    <?php
+      echo ("<b>ตารางเรียน ปีการศึกษา 2/2563<b><br/>");
+      echo ("<b>คณะเทคโนโลยีและการจัดการอุตสาหกรรม<b><br/>");
+      echo ("<b>หลักสูตร 62060214 หลักสูตรวิทยาศาสตร์บัณฑิต สาขาวิชาเทคโนโลยีสารสนเทศ<b><br/>");
+      echo ("<b>นักศึกษา:2RA รอบเช้า<b><br/>");
+    ?>
+    <table  border= 0.5 align="center" bgcolor ="black"> <!–กรอบ–>
+    <tr style="height:30px;">
+        <th style="width:50px; " <table  border=0.5 align="center" bgcolor ="hotpink">Day</th>
+        <th style="width:80px;"  <table  border=0.5 align="center" bgcolor ="hotpink">8:00</th>
+        <th style="width:80px; " <table  border=0.5 align="center" bgcolor ="hotpink">9:00</th>
+        <th style="width:80px; " <table  border=0.5 align="center" bgcolor ="hotpink">10:00</th>
+        <th style="width:80px; " <table  border=0.5 align="center" bgcolor ="hotpink">11:00</th>
+        <th style="width:80px; " <table  border=0.5 align="center" bgcolor ="hotpink">12:00</th>
+        <th style="width:80px; " <table  border=0.5 align="center" bgcolor ="hotpink">13:00</th>
+        <th style="width:80px; " <table  border=0.5 align="center" bgcolor ="hotpink">14:00</th>
+        <th style="width:80px; " <table  border=0.5 align="center" bgcolor ="hotpink">15:00</th>
+        <th style="width:80px; " <table  border=0.5 align="center" bgcolor ="hotpink">16:00</th>
+        <th style="width:80px; " <table  border=0.5 align="center" bgcolor ="hotpink">17:00</th>
+        <th style="width:80px; " <table  border=0.5 align="center" bgcolor ="hotpink">18:00</th>
+        <th style="width:80px; " <table  border=0.5 align="center" bgcolor ="hotpink">19:00</th>
+        <th style="width:80px; " <table  border=0.5 align="center" bgcolor ="hotpink">20:00</th>
+    </tr>
+
+    </tr>
+
+
+
+
+</center> 
+
+</body>
+</html>
