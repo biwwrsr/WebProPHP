1f9fbe60b55e91d697e7f24d47d5dcdb73f31f51
biwwrsr <6206021620132@fitm.kmutnb.ac.th>
2021-01-17 10:44:03 +0700

    table2RA

--- ฝึกเขียน/table2RA.php
+++ ฝึกเขียน/table2RA.php
@@ -25,10 +25,74 @@
         <th style="width:80px; " <table  border=0.5 align="center" bgcolor ="hotpink">17:00</th>
         <th style="width:80px; " <table  border=0.5 align="center" bgcolor ="hotpink">18:00</th>
         <th style="width:80px; " <table  border=0.5 align="center" bgcolor ="hotpink">19:00</th>
-        <th style="width:80px; " <table  border=0.5 align="center" bgcolor ="hotpink">20:00</th>
-    </tr>
+        <th style="width:80px; " <table  border=0.5 align="center" bgcolor ="hotpink">20:00</th></tr>
 
-    </tr>
+    <tr style="height:60px;"> <!–วันจันทร์–>
+        <th style="width:50px; " <table  border=0.5 align="center" bgcolor ="hotpink">M</th>
+        <td colspan=5 bgcolor="lightgray"> </td>
+        <td colspan=3 bgcolor="lightpink"><font size="3">060243111&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;S.1<br><br>06-SKC&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*1-POUNGKAM2</font></td>
+        <td colspan=1 bgcolor="lightgray"> </td>
+        <td colspan=3 bgcolor="lightpink"><font size="3">061100001&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;S.2<br><br>06-YPP, 06-NPN&nbsp;&nbsp;*1-POUNGKAM2</font></td>
+        <td colspan=1 bgcolor="lightgray"> </td>
+
+    <tr style="height:60px;"> <!–วันอังคาร–>
+        <th style="width:50px; " <table  border=0.5 align="center" bgcolor ="hotpink">T</th>
+        <td colspan=1 bgcolor="lightgray"> </td>
+        <td colspan=3 bgcolor="lightpink"><font size="3">080103034&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;S.3<br><br>08-SPK&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*1-B3-11 </font></td>
+        <td colspan=5 bgcolor="lightgray"> </td>
+        <td colspan=3 bgcolor="lightpink"><font size="3">060243112&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;S.1<br><br>06-PSK&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*1-POUNGKAM2</font></td>
+        <td colspan=1 bgcolor="lightgray"> </td>
+
+
+    <tr style="height:60px;"> <!วันพุธ>
+        <th style="width:50px; " <table  border=0.5 align="center" bgcolor ="hotpink">W</th>
+        <td colspan=2 bgcolor="lightpink"><font size="3">060243113&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;S.1<br><br>06-NKT&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*1-B4-01-A</font></td>
+        <td colspan=2 bgcolor="lightpink"><font size="3">060243113&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;S.1<br><br>06-NKT&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*1-B4-01-A</font></td>
+        <td colspan=4 bgcolor="lightgray"> </td>
+        <td colspan=2 bgcolor="lightpink"><font size="3">060243110&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;S.1<br><br>06-AMK&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*1-B4-09 </font></td>
+        <td colspan=2 bgcolor="lightpink"><font size="3">060243110&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;L.1<br><br>06-AMK&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*1-B4-09 </font></td>
+        <td colspan=1 bgcolor="lightgray"> </td>
+       
+        
+    <tr style="height:60px;">
+        <th style="width:50px; " <table  border=0.5 align="center" bgcolor ="hotpink">H</th>
+        <td colspan=13 bgcolor="lightgray"> </td>
+    <tr style="height:60px;">
+        <th style="width:50px; " <table  border=0.5 align="center" bgcolor ="hotpink">F</th>
+        <td colspan=13 bgcolor="lightgray"> </td>
+
+  
+
+
+    
+    
+
+
+    
 
 
 
