3f490906dba3375e4f34d73020f481af6cc3f13c
biwwrsr <6206021620132@fitm.kmutnb.ac.th>
2021-01-18 19:14:51 +0700

    update

--- ฝึกเขียน/page1.php
+++ ฝึกเขียน/page1.php
@@ -5,16 +5,29 @@
 <body>
 <center>
     <?php
+        echo "<b>แบบฟอร์มบันทึกข้อมูล</b><br />";
         echo "<img src='cat.png' ", "width=120,height=110 /><br>";
-        echo "ชื่อ :";
-        echo " <input type=text name=user><br>";
-        echo "นามสกุล :";
-        echo " <input type=text name=user><br>";
-        echo "E-mail : ";
-        echo " <input type=text name=user><br>";
-        echo "Password :";
-        echo " <input type=text name=user><br>";
-        echo "<a href='page2.php'> ต่อไป</a>";
+        echo "ชื่อ : <input type=text ><br>";
+        echo "นามสกุล : <input type=text ><br>";
+        echo "<form>
+        วุฒิการศึกษา :
+        <select>
+        <option>ประถมศึกษา
+        <option>มัธยมศึกษา
+        <option>ปริญญาตรี
+        <option>สูงกว่าปริญญาตรี
+        </select>
+        </form>";
+        echo "E-mail : <input type=text ><br>";
+        echo "Password : <input type=password ><br>";
+        echo "เพศ : <input type=radio name=sex value=1 /> ชาย
+            <input type=radio name=sex value=2 /> หญิง <br />";
+        echo "งานอดิเรก :
+        <input type=checkbox value=1 /> อ่านหนังสือ
+        <input type=checkbox value=2 /> ดูโทรทัศน์
+        <input type=checkbox value=3 /> เล่นกีฬา<br />";
+        echo "<a href='page2.php'> <input type=button value=submit></a>";
+        
     ?>
 </center> 
 </body>
