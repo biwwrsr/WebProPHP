841a67374ce550ed611763b8558ab5558037465f
biwwrsr <6206021620132@fitm.kmutnb.ac.th>
2021-01-19 19:28:04 +0700

    calsquare

---
+++ ฝึกเขียน/calsquare.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<title>โปรแกรมคำนวณพื้นที่สี่เหลี่ยม</title>
+</head>
+<body>
+<center>
+<form method= "POST">
+    <p><b>คำนวณพื้นที่สี่เหลี่ยม</b><br>
+        กว้าง :<input name= "width">ตารางเซนติเมตร<br>
+       ยาว: <input name= "height">ตารางเซนติเมตร<br>
+       <input name= "submit" type= "submit" value= "เลือก">
+    </p>
+</form>
+
+<?php
+$width = @$_POST['width'] ;
+$height = @$_POST['height'];
+
+echo "สี่หลี่ยมนี้มีพื้นที่ ".$width*$height." ตารางเซนติเมตรค่ะ";
+ ?>
+ </center>
+</body>
+</html>
