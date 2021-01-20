9b7a10353b4a4f5a613529022b9cc2c8a28fa99a
biwwrsr <6206021620132@fitm.kmutnb.ac.th>
2021-01-20 12:27:49 +0700

    update

--- workclass/testfrom.php
+++ workclass/testfrom.php
@@ -1,14 +1,12 @@
 <!DOCTYPE HTML>
 <html>  
-<body>
+    <body>
 
-<form action="welcome_get.php" method="get">
-    Num X <input type="text" num="x"><br>
-    Num Y: <input type="e-mail" num="y"><br>
-    Operator : <input type="text" opertor ="o"><br>
-    <input type="submit">
-</form>
-
-
-</body>
+    <form action="welcome_get.php" method="get">
+        Num x: <input type="number" name="x"><br>
+        Num Y: <input type="number" name="y"><br>
+        Operator : <input type="text" name="o"><br>
+        <input type="submit">
+    </form>
+    </body>
 </html>
