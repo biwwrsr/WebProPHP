1e563a067132ad09719acc45b907e5e3d06742ea
biwwrsr <6206021620132@fitm.kmutnb.ac.th>
2021-02-15 22:58:42 +0700

    update

--- Sheet/Lab7/7_6.php
+++ Sheet/Lab7/7_6.php
@@ -6,7 +6,7 @@ foreach($files as $value){
 echo $value."<br>";
 }
 echo getcwd()."<br>";
-chdir("..\..\..\..\\");//ถอยหลังไป1ชั้น
+chdir("..\..\..\..\\");//ถอยหลัง
 echo getcwd()."<br>";
 chdir("htdocs\WebProPHP\Sheet\Lab7");
 echo getcwd();
