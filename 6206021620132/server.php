cd61e846260b17e2d02bb74de1a56fecb76a6ca9
biwwrsr <52157633+biwwrsr@users.noreply.github.com>
2021-04-07 08:57:05 +0700

    Add files via upload

---
+++ 6206021620132/server.php
@@ -0,0 +1,14 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "register_db";
+
+    //สร้างการเชื่อมต่อ
+    $conn = mysqli_connect($servername,$username,$password,$dbname);
+    //เช็คการเชื่อมต่อ
+    if(!$conn){
+        die("Connection failed" . mysqli_connect_error());
+    }
+
+?>
